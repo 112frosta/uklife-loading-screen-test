561791d75e21c1a9f432e51b0e493e0fba438ac2
112frosta <112frosta@proton.me>
2024-05-27 14:46:22 +0200

    first commit

---
+++ ui/src/App.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: "red",
+          color: "white",
+          padding: 10,
+          height: 100,
+        }}
+      >
+        Cwel
+      </div>
+    </>
+  );
+}
